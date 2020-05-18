9ed4414e0d0522dfbbd32b00801621378f16049e
Patricia Aas <psmaas@gmail.com>
2020-05-18 23:17:58 +0200

    Update restore.sh

--- restore.sh
+++ restore.sh
@@ -7,16 +7,14 @@ echo "Using cache $INPUT_CACHE_NAME"
 mkdir -p $CONAN_USER_HOME
 cd $CONAN_USER_HOME
 
-# 1. Check out cache - shallow and fetch
+# 1. Check out cache
+#    If it fails - exit 1
 echo "Checking out at $CONAN_USER_HOME"
-BRANCH=master
-git clone https://${INPUT_BOT_NAME}:${INPUT_BOT_TOKEN}@github.com/${INPUT_CACHE_NAME}.git ${CONAN_USER_HOME} --branch=${BRANCH}
-# If it fails - exit 1
+git clone https://${INPUT_BOT_NAME}:${INPUT_BOT_TOKEN}@github.com/${INPUT_CACHE_NAME}.git ${CONAN_USER_HOME} --branch=master || exit 1
 
 # 2. Check if explicit key exits
+#    If it does - check out explicit and set cache_hit to 1
 echo "Trying explicit key $INPUT_KEY"
-
-# If it does - check out explicit and set cache_hit to 1
 if [ $(git tag -l "$INPUT_KEY") ]; then
     git checkout ${INPUT_KEY}
     echo "::set-output name=cache-hit::1"
@@ -24,10 +22,9 @@ if [ $(git tag -l "$INPUT_KEY") ]; then
 fi
 
 # 3. If it doesn't check if fallback exits
+#    If it does - check out fallback and set cache_hit to 2
 FALLBACK_KEY="host-${RUNNER_OS}-target-${RUNNER_OS}-${BRANCH}"
 echo "Trying fallback key $FALLBACK_KEY"
-
-# If it does - check out fallback and set cache_hit to 2
 if [ $(git tag -l "$FALLBACK_KEY") ]; then
     git checkout ${FALLBACK_KEY}
     echo "::set-output name=cache-hit::2"
