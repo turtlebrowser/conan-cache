17499d8de26e6262515adac6d45a38f8c44128e7
Patricia Aas <psmaas@gmail.com>
2020-05-18 14:16:18 +0200

    Update save.sh

--- save.sh
+++ save.sh
@@ -4,4 +4,11 @@ echo "Path $1"
 echo "Explicit key $2"
 echo "Fallback key $3"
 cache_hit=1
+
+# If no changes were made - exit 0
+
+# if cache_hit was 1 (explicit) - should you update?
+
+# If cache_hit was 0 (miss) or 2 (fallback) - make a new explicit
+
 echo "::set-output name=cache-hit::$cache_hit"
