9053be5520c6d5e09417d661b00809c1466d5110
Patricia Aas <psmaas@gmail.com>
2020-05-21 09:32:03 +0200

    Move over to JS action

--- README.md
+++ README.md
@@ -42,3 +42,23 @@ Cache a hit on a key: no hit (0), explicit key (1), fallback key (2)
 * Give your machine-user rights on your cache repo
 * Create a personal access token for your machine-user [https://help.github.com/en/github/authenticating-to-github/creating-a-personal-access-token-for-the-command-line]
 * Add the token as a secret to your workflow [https://help.github.com/en/actions/configuring-and-managing-workflows/creating-and-storing-encrypted-secrets]
+
+## Make an empty repo
+~~~
+git clone git@github.com:turtlebrowser/conan-cache.git
+mkdir .conan
+touch .conan/conan-cache.marker
+git add .conan/conan-cache.marker
+git commit -m "Setup cache"
+git push
+touch .gitattributes && git add .gitattributes && git commit -m "Add .gitattributes" && git push
+~~~
+
+Add the bot account as collaborator on the repo
+
+## Notes
+
+Might be needed to pull
+~~~
+git lfs pull
+~~~
---
+++ restore.js
@@ -0,0 +1,49 @@
+// From https://github.com/ad-m/github-push-action
+// 
+// MIT License
+// 
+// Copyright (c) 2019 Adam Dobrawy
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+const spawn = require('child_process').spawn;
+const path = require("path");
+
+const exec = (cmd, args=[]) => new Promise((resolve, reject) => {
+    console.log(`Started: ${cmd} ${args.join(" ")}`)
+    const app = spawn(cmd, args, { stdio: 'inherit' });
+    app.on('close', code => {
+        if(code !== 0){
+            err = new Error(`Invalid status code: ${code}`);
+            err.code = code;
+            return reject(err);
+        };
+        return resolve(code);
+    });
+    app.on('error', reject);
+});
+
+const main = async () => {
+    await exec('bash', [path.join(__dirname, './restore.sh')]);
+};
+
+main().catch(err => {
+    console.error(err);
+    console.error(err.stack);
+    process.exit(err.code || -1);
+})
