1c988301808e8729fdb0cae6abd51477e10271ee
Patricia Aas <patricia@turtlesec.no>
2020-09-13 14:54:59 +0200

    Change branch name to new format

--- update_cache_windows.sh
+++ update_cache_windows.sh
@@ -36,7 +36,7 @@ export LFS_LIMIT=50
 
 header "Prepare Cache for update"
 
-confirm "Clean cache? [y/N]" && cd ${CONAN_USER_HOME} && git clean -df && git checkout . && git checkout host-Windows-target-Windows-master
+confirm "Clean cache? [y/N]" && cd ${CONAN_USER_HOME} && git clean -df && git checkout . && git checkout host-Windows-target-Windows-windows-latest-master
 
 confirm "Update cache? [y/N]" && cd ${CONAN_USER_HOME} && git pull && git lfs pull
 
