9ca1b65052b689276c4d5a08178d4f90627f9a7b
Patricia Aas <psmaas@gmail.com>
2020-12-04 13:10:12 +0100

    Try to restructure cache-hit

--- restore.sh
+++ restore.sh
@@ -17,6 +17,10 @@ git clone https://${INPUT_BOT_NAME}:${INPUT_BOT_TOKEN}@github.com/${INPUT_CACHE_
 echo "-- Conan Cache: Enable long paths"
 git config --global core.longpaths true
 
+hit_cache=0
+echo "-- Conan Cache: Setting preliminary cache-hit $hit_cache"
+echo "::set-output name=cache-hit::$hit_cache"
+
 # Check if explicit key exits
 echo "-- Conan Cache: Trying explicit key $INPUT_KEY"
 if [ $(git tag --list "$INPUT_KEY") ]; then
@@ -24,7 +28,8 @@ if [ $(git tag --list "$INPUT_KEY") ]; then
     git checkout ${INPUT_KEY} || exit 1
     echo "-- Conan Cache: replace CONAN_USER_HOME_SHORT with ${CONAN_USER_HOME_SHORT}"
     find .conan/ -name .conan_link -exec perl -pi -e 's=CONAN_USER_HOME_SHORT=$ENV{CONAN_USER_HOME_SHORT}=g' {} +
-    echo "::set-output name=cache-hit::1"
+    #echo "::set-output name=cache-hit::1"
+    hit_cache=1
 else
     # If it doesn't check if fallback exits
     FALLBACK_KEY="host-${RUNNER_OS}-target-${INPUT_TARGET_OS}-${REPO_BRANCH}"
@@ -40,12 +45,17 @@ else
         git lfs pull || exit 1
         echo "-- Conan Cache: replace CONAN_USER_HOME_SHORT with ${CONAN_USER_HOME_SHORT}"
         find .conan/ -name .conan_link -exec perl -pi -e 's=CONAN_USER_HOME_SHORT=$ENV{CONAN_USER_HOME_SHORT}=g' {} +
-        echo "::set-output name=cache-hit::2"
+        #echo "::set-output name=cache-hit::2"
+        hit_cache=2
     else
         # If it doesn't - create the branch and set cache_hit to 0
         echo "-- Conan Cache: Creating fallback key $FALLBACK_KEY"
         git checkout -b ${FALLBACK_KEY} || exit 1
         git push -u origin ${FALLBACK_KEY} || exit 1
-        echo "::set-output name=cache-hit::0"
+        #echo "::set-output name=cache-hit::0"
+        hit_cache=0
     fi
 fi
+
+echo "-- Conan Cache: Setting cache-hit $hit_cache"
+echo "::set-output name=cache-hit::$hit_cache"
