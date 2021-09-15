44601752569034ffde2250f0ae9d2e6fc346c382
Patricia Aas <patricia@turtlesec.com>
2021-09-15 09:28:12 +0200

    Can't use local

--- restore.sh
+++ restore.sh
@@ -34,7 +34,7 @@ else
     FALLBACK_KEY="host-${RUNNER_OS}-target-${INPUT_TARGET_OS}-${REPO_BRANCH}"
     echo "-- Conan Cache: Trying fallback key $FALLBACK_KEY"
 
-    local fallback_exists=$(git ls-remote origin ${FALLBACK_KEY})
+    fallback_exists=$(git ls-remote --heads origin ${FALLBACK_KEY})
 
     if [[ -z ${fallback_exists} ]]; then
         # If it does - check out fallback and set cache_hit to 2
